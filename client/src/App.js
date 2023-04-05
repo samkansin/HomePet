1b9533f354c10e2b4e06bdce481b1dd25f108d09
knightsmsk <knightsmsk@gmail.com>
2023-04-06 05:15:27 +0700

    update font | update card | update Homepage | update basecolor

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,14 @@ import {
 } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
 
 const HomePetRouter = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<MainLayout />}></Route>)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
 );
 
 const App = () => <RouterProvider router={HomePetRouter} />;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,25 @@
+import Card from '../components/Card';
+import '../CSS/Home.css';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='Home-title'>
+        <i className='icon-active' />
+        <span>Our Pet</span>
+        <i className='icon-active' />
+      </div>
+      <div className='card-list'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
