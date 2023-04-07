d6f3b5205678b2971d8dd5755a888c5b91e4fd57
SONTAYA JAHMUENPRABNAKORN <sontaya.jahm@gmail.com>
2023-04-07 23:27:20 +0700

    add localforage: store and load data

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
-import Adopt from './pages/Adopt';
+import Adopt, { petsLoader } from './pages/Adopt';
 import Index from './pages/Index';
 import Post from './pages/Post';
 
@@ -14,7 +14,7 @@ const HomePetRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<Index />} />
-      <Route path='adopt' element={<Adopt />} />
+      <Route path='adopt' element={<Adopt />} loader={petsLoader}/>
       <Route path='post' element={<Post />} />
     </Route>
   )
--- client/src/pages/Adopt.js
+++ client/src/pages/Adopt.js
@@ -1,74 +1,11 @@
 import '../CSS/Adopt.css';
 import PetCard from '../components/PetCard';
-
-const PetData = [
-  {
-    id: 1,
-    image_src:
-      'https://www.central.co.th/e-shopping/storage/2020/12/CUTE-KITTY.jpg',
-    name: 'Khunpan',
-    type: 'Cat',
-    breed: 'American Shorthair',
-    details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit.',
-    ageMonth: 3,
-    ageYear: 0,
-    gender: 'Female',
-    status: 'Available',
-    owner: 'David C.',
-    time: '6 hours ago',
-  },
-  {
-    id: 2,
-    image_src:
-      'https://cdn.onemars.net/sites/whiskas_th_r81SA_mwh5/image/thumb_cat-breeds-01_1652358723718.jpeg',
-    name: 'Bella',
-    type: 'Cat',
-    breed: 'Thai Cat',
-    details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit.',
-    ageMonth: 2,
-    ageYear: 0,
-    gender: 'Male',
-    status: 'Adopted',
-    owner: 'James B.',
-    time: '1 day ago',
-  },
-  {
-    id: 3,
-    image_src:
-      'https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDF8fHdvcmtpbmclMjBkZXNrfGVufDB8fHx8MTYyNjI1MDYwMg&ixlib=rb-1.2.1&w=600',
-    name: 'Mumu',
-    type: 'Dog',
-    breed: 'Bulldog',
-    details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit.',
-    ageMonth: 6,
-    ageYear: 0,
-    gender: 'Female',
-    status: 'Available',
-    owner: 'George D.',
-    time: '2 days ago',
-  },
-  {
-    id: 4,
-    image_src:
-      'https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDF8fHdvcmtpbmclMjBkZXNrfGVufDB8fHx8MTYyNjI1MDYwMg&ixlib=rb-1.2.1&w=600',
-    name: 'Mew',
-    type: 'Cat',
-    breed: 'Bengal',
-    details:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit. adipiscing elit adipiscing elit adipiscing elit.',
-    ageMonth: 10,
-    ageYear: 0,
-    gender: 'Male',
-    status: 'Available',
-    owner: 'Lily M. ',
-    time: '3 days ago',
-  },
-];
+import { useLoaderData } from 'react-router-dom';
+import { getPetData } from '../petsData';
 
 const Adopt = () => {
+  const PetData = useLoaderData();
+
   return (
     <div className='Home'>
       <div className='Home-title'>
@@ -102,3 +39,8 @@ const Adopt = () => {
 };
 
 export default Adopt;
+
+export const petsLoader = async () => {
+  const res = await getPetData();
+  return res;
+};
